14c582d1554ca791884b7add67304ab1357214a5
Rencas <renzo.c@ohecommerce.agency>
2023-04-25 20:37:14 -0500

    uptask init backend

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+console.log('desdes index js');
+
+app.listen(4000, () => {
+  console.log('Servidor corriendo en el puerto 4000');
+});
