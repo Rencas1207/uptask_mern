e5b9fb493d097531e5f2a453ac3fc06003e212dd
Rencas <rencasdag.12@gmail.com>
2023-07-08 16:00:25 -0500

    sending request, reading dates & creating users + prevent duplicated users

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -1,4 +1,24 @@
-const registrar = (req, res) => {
+import Usuario from '../models/Usuario.js'
+
+const registrar = async (req, res) => {
+   // evitar registros duplicados
+   const { email } = req.body;
+   const existeUsuario = await Usuario.findOne({ email: email });
+
+   if (existeUsuario) {
+      const error = new Error('Usuario ya registrado');
+      return res.status(400).json({ msg: error.message })
+   }
+   console.log(existeUsuario);
+
+   try {
+      const usuario = new Usuario(req.body);
+      const usuarioAlmacenado = await usuario.save();
+      res.json(usuarioAlmacenado);
+   } catch (error) {
+      console.log(error);
+   }
+
 
 }
 
