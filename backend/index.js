967a17630be3fe8db5529cee6d965568878eb16d
Rencas <rencasdag.12@gmail.com>
2023-07-22 19:50:14 -0500

    creating projects

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ conectarDB();
 
 // Routing
 app.use("/api/usuarios", usuarioRoutes);
-app.use("/api/proyectos", usuarioRoutes);
+app.use("/api/proyectos", proyectoRoutes);
 
 const PORT = process.env.PORT || 4000;
 
