7d8e926ab7ac57c7c768848e8872f98d29f88fac
Rencas <rencasdag.12@gmail.com>
2023-07-24 22:06:25 -0500

    creating the forget password + new password + confirm account pages

--- frontend/src/pages/ForgetPassword.jsx
+++ frontend/src/pages/ForgetPassword.jsx
@@ -1,5 +1,51 @@
+import { Link } from 'react-router-dom';
+
 const ForgetPassword = () => {
-  return <div>ForgetPassword</div>;
+  return (
+    <>
+      <h1 className="text-sky-600 font-black text-4xl">
+        Recupera tu acceso y no pierdas tus{' '}
+        <span className="text-slate-700"> proyectos</span>
+      </h1>
+      <form className="my-10 bg-white shadow rounded-lg px-10 py-5">
+        <div className="my-5">
+          <label
+            htmlFor="email"
+            className="uppercase text-gray-600 block text-xl font-bold"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Email de registro"
+            className="w-full mt-3 p-3 outline-none rounded-xl bg-gray-50"
+          />
+        </div>
+
+        <input
+          type="submit"
+          value="Enviar instrucciones"
+          className="bg-sky-700 w-full py-3 mb-5 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
+        />
+      </form>
+
+      <nav className="lg:flex lg:justify-between ">
+        <Link
+          to="/"
+          className="block text-center my-5 text-slate-500 uppercase text-sm"
+        >
+          ¿Ya tienes una cuenta? Inicia Sesión{' '}
+        </Link>
+        <Link
+          to="/register"
+          className="block text-center my-5 text-slate-500 uppercase text-sm"
+        >
+          ¿No tienes una cuenta? Regístrate{' '}
+        </Link>
+      </nav>
+    </>
+  );
 };
 
 export default ForgetPassword;
--- frontend/src/pages/NewPassword.jsx
+++ frontend/src/pages/NewPassword.jsx
@@ -1,5 +1,34 @@
 const NewPassword = () => {
-  return <div>NewPassword</div>;
+  return (
+    <>
+      <h1 className="text-sky-600 font-black text-4xl">
+        Reestablece tu password y no pierdas acceso a tus{' '}
+        <span className="text-slate-700"> proyectos</span>
+      </h1>
+      <form className="my-10 bg-white shadow rounded-lg px-10 py-5">
+        <div className="my-5">
+          <label
+            htmlFor="password"
+            className="uppercase text-gray-600 block text-xl font-bold"
+          >
+            Nuevo Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Escribe tu Nuevo Password"
+            className="w-full mt-3 p-3 outline-none rounded-xl bg-gray-50"
+          />
+        </div>
+
+        <input
+          type="submit"
+          value="Guardar Nuevo Password"
+          className="bg-sky-700 w-full py-3 mb-5 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
+        />
+      </form>
+    </>
+  );
 };
 
 export default NewPassword;
