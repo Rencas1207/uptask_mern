86fd4746959f0b6e88d58f7bd9cf6423fe502a1a
Rencas <rencasdag.12@gmail.com>
2023-08-12 12:48:53 -0500

    creating form for search a collaborator

--- frontend/src/components/Alert.jsx
+++ frontend/src/components/Alert.jsx
@@ -3,7 +3,7 @@ const Alert = ({ alert }) => {
     <div
       className={`${
         alert.error ? 'from-red-400 to-red-600' : 'from-sky-400 to-sky-600'
-      } bg-gradient-to-br text-center p-3 rounded-xl uppercase text-white font-bold text-sm my-10`}
+      } bg-gradient-to-br text-center p-3 rounded-xl uppercase text-white font-bold text-sm mb-10`}
     >
       {alert.msg}
     </div>
