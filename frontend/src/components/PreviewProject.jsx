e46e09bb63930760b72c2ce7700b97b95aee3c50
Rencas <rencasdag.12@gmail.com>
2023-08-13 19:43:38 -0500

    show collaborator  tag when not owning a project

--- frontend/src/components/PreviewProject.jsx
+++ frontend/src/components/PreviewProject.jsx
@@ -1,15 +1,25 @@
 import { Link } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
 
 const PreviewProject = ({ project }) => {
-  const { name, _id, client } = project;
+  const { auth } = useAuth();
+  const { name, _id, client, creador } = project;
   return (
-    <div className="border-b p-5 flex">
-      <p className="flex-1">
-        {name}
-        <span className="text-sm text-gray-500 uppercase">
-          {''} {client}
-        </span>
-      </p>
+    <div className="border-b p-5 flex justify-between">
+      <div className="flex items-center gap-2">
+        <p className="flex-1">
+          {name}
+          <span className="text-sm text-gray-500 uppercase">
+            {''} {client}
+          </span>
+        </p>
+
+        {auth._id !== creador && (
+          <p className="py-1 px-2 text-xs  rounded-lg text-white bg-green-500 font-bold uppercase">
+            Colaborador
+          </p>
+        )}
+      </div>
       <Link
         to={`${_id}`}
         className="text-gray-600 hover:text-gray-800 uppercase text-sm font-bold"
